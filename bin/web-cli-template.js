0f1f8a5093247b7b87925336e8324cda7a995019
daiyi <daiyi.friday@ad.bytedance.com>
2020-04-20 17:33:11 +0800

    add template for mobx

--- bin/web-cli-template.js
+++ bin/web-cli-template.js
@@ -1,13 +1,71 @@
 #!/usr/bin/env node
-
-const path = require('path');
+const fs = require('fs');
 const program = require('commander');
-const download = require('../lib/download');
+
+const template = {
+    'react': `
+import React, { Component } from 'react';
+
+export default class Index extends Component{
+    constructor(props) {
+    super(props);
+    this.state = {
+        //
+    };
+}
+
+render() {
+    return (
+        <div></div>
+    );
+}
+    }`,
+    'hook': `
+import React from 'react';
+
+const Component = () => {
+    return (
+        <div>
+            hook
+        </div>
+    );
+}
+
+export default React.memo(Component);
+    `,
+    'hook-mobx': `
+import React from 'react';
+import { inject, observer } from 'mobx-react';
+
+const Component = observer(() => {
+    return (
+        <div>
+            hook-mobx
+        </div>
+    );
+});
+export default React.memo(inject()(Component));
+`
+};
 
 program.usage('<template>').parse(process.argv);
 
 // 根据输入，获取template名称
 let projectName = program.args[0];
+let type;
+switch (projectName) {
+    case 'react':
+        type = template.react;
+        break;
+    case 'hook':
+        type = template.hook;
+        break;
+    case 'hook-mobx':
+        type = template["hook-mobx"];
+        break;
+    default:
+        type = template.react;
+}
 
 if (!projectName) {  // project-name 必填
     // 相当于执行命令的--help选项，显示help信息，这是commander内置的一个命令选项
@@ -18,8 +76,11 @@ if (!projectName) {  // project-name 必填
 go();
 
 function go() {
-    console.log('start template', projectName);
-    download('https://github.com:FridayDai/react-mobx-scaffold#template-react', path.resolve(__dirname))
-        .then(target => console.log('finish template ', target))
-        .catch(xhr => console.log(xhr));
+    fs.writeFile('index.js', type, 'utf8', function(error) {
+        if(error) {
+            console.log(error);
+            return false;
+        }
+        console.log('success');
+    });
 }
